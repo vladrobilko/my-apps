59cc60b992032606bf611ad051325ac7d958fb45
vladrobilko <vladrobilko@gmail.com>
2023-04-03 17:37:07 +0400

    add styles

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import AlgebraicExpressions from "./Components/AlgebraicExpressions";
 import CarTrip from "./Components/CarTrip";
 import CounterWithAddAndDelete from "./Components/CounterWithAddAndDelete";
 import ToDoWithFirebase from "./Components/ToDoWithFirebase";
+import background from './background.jpg';
+
 
 function App() {
 
     return (
-        <div className='container'>
-            <h1 style={{ color: 'forestgreen' }}>My small apps with react</h1>
+        <div style={{backgroundImage: `url(${background})`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat'  }}>
+            <div className='container'>
+            <h1 class="text-center" style={{color: 'forestgreen'}}>My small apps with react</h1>
             <ToDoWithFirebase/>
             <AlgebraicExpressions/>
             <CarTrip/>
             <CounterWithAddAndDelete/>
+            </div>
         </div>
     );
 }
